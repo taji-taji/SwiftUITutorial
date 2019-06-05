571825b53bed823536f312b2833de010b308eb44
Yutaka Tajika <y.tajika529@gmail.com>
2019-06-05 21:55:55 +0900

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 多鹿豊 on 2019/06/05.
+//  Copyright © 2019 多鹿豊. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
