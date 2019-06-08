c2d3f6015f9acec50ff9d290acab9eeb7387fb68
Yutaka Tajika <y.tajika529@gmail.com>
2019-06-09 07:18:34 +0900

    Section 4 : Building Lists and Navigation

---
+++ Landmarks/LandmarkList.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+struct LandmarkList : View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarkData[0])
+            LandmarkRow(landmark: landmarkData[1])
+        }
+    }
+}
+
+#if DEBUG
+struct LandmarkList_Previews : PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
+#endif
