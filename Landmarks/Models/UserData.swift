37123241c44f2d6299f2152a70cf8110d5142fdc
Yutaka Tajika <y.tajika529@gmail.com>
2019-06-10 09:03:41 +0900

    Handling User Input : Section 4

---
+++ Landmarks/Models/UserData.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+import Combine
+
+final class UserData: BindableObject {
+    let didChange = PassthroughSubject<UserData, Never>()
+    
+    var showFavoritesOnly = false {
+        didSet {
+            didChange.send(self)
+        }
+    }
+    
+    var landmarks = landmarkData {
+        didSet {
+            didChange.send(self)
+        }
+    }
+}
