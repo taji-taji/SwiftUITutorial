2a3166b58a11b66d60b3328139aef770c125a797
Yutaka Tajika <y.tajika529@gmail.com>
2019-06-10 09:12:45 +0900

    Handling User Input : Section 5

--- Landmarks/LandmarkDetail.swift
+++ Landmarks/LandmarkDetail.swift
@@ -1,7 +1,13 @@
 import SwiftUI
 
 struct LandmarkDetail : View {
+    @EnvironmentObject var userData: UserData
+    
     var landmark: Landmark
+    
+    var landmarkIndex: Int {
+        userData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
     
     var body: some View {
         VStack {
@@ -36,6 +42,7 @@ struct LandmarkDetail : View {
 struct ContentView_Previews : PreviewProvider {
     static var previews: some View {
         LandmarkDetail(landmark: landmarkData[0])
+            .environmentObject(UserData())
     }
 }
 #endif
--- Landmarks/LandmarkList.swift
+++ Landmarks/LandmarkList.swift
@@ -1,17 +1,17 @@
 import SwiftUI
 
 struct LandmarkList : View {
-    @State var showFavoritesOnly =  false
+    @EnvironmentObject var userData: UserData
     
     var body: some View {
         NavigationView {
             List {
-                Toggle(isOn: $showFavoritesOnly) {
+                Toggle(isOn: $userData.showFavoritesOnly) {
                     Text("Favorites only")
                 }
                 
-                ForEach(landmarkData) { landmark in
-                    if !self.showFavoritesOnly || landmark.isFavorite {
+                ForEach(userData.landmarks) { landmark in
+                    if !self.userData.showFavoritesOnly || landmark.isFavorite {
                         NavigationButton(destination: LandmarkDetail(landmark: landmark)) {
                             LandmarkRow(landmark: landmark)
                         }
@@ -27,6 +27,7 @@ struct LandmarkList : View {
 struct LandmarkList_Previews : PreviewProvider {
     static var previews: some View {
         LandmarkList()
+            .environmentObject(UserData())
     }
 }
 #endif
